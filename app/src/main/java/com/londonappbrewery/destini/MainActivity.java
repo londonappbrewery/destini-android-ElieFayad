f737a7455bb08e930e1e73f51f80afeb97032ed4
ElieFayad <elie.fayad98@gmail.com>
2018-08-01 12:59:35 +0300

    Section 9 challenge: completed

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -11,11 +11,11 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-    int mIndex;
-    private TextView mStoryTextView;
+    int mIndex;//to keep track of the story
+    private TextView mStoryTextView;//tells the story
     private TextView mUserPrompt;
-    private Button mTopButton;
-    private Button mBottomButton;
+    private Button mTopButton;//choice #1
+    private Button mBottomButton;//choice #2
 
 
     @Override
@@ -49,13 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void updateAtEnd(){
+    private void updateAtEnd(){//update when arrived at the end of the story (hide buttons and change the text of the user prompt)
         mTopButton.setVisibility(View.GONE);
         mBottomButton.setVisibility(View.GONE);
         mUserPrompt.setText(R.string.end);
     }
 
-    private void updateTop(){
+    private void updateTop(){//when choice #1 is selected go to the corresponding storyline according to mIndex
         switch(mIndex){
             case 1: case 2:mStoryTextView.setText(R.string.T3_Story);
                            mTopButton.setText(R.string.T3_Ans1);
@@ -63,31 +63,28 @@ public class MainActivity extends AppCompatActivity {
                            mIndex=3;
                            break;
             case 3:mStoryTextView.setText(R.string.T6_End);
-                updateAtEnd();
-                mIndex=6;
-                break;
+                   updateAtEnd();
+                   mIndex=6;
+                   break;
         }
 
     }
 
-    private void updateBottom(){
+    private void updateBottom(){//when choice #2 is selected go to the corresponding storyline according to mIndex
         switch (mIndex) {
-            case 1:
-                mStoryTextView.setText(R.string.T2_Story);
-                mTopButton.setText(R.string.T2_Ans1);
-                mBottomButton.setText(R.string.T2_Ans2);
-                mIndex = 2;
-                break;
-            case 2:
-                mStoryTextView.setText(R.string.T4_End);
-                updateAtEnd();
-                mIndex = 4;
-                break;
-            case 3:
-                mStoryTextView.setText(R.string.T5_End);
-                updateAtEnd();
-                mIndex = 5;
-                break;
+            case 1: mStoryTextView.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mBottomButton.setText(R.string.T2_Ans2);
+                    mIndex = 2;
+                    break;
+            case 2: mStoryTextView.setText(R.string.T4_End);
+                    updateAtEnd();
+                    mIndex = 4;
+                    break;
+            case 3: mStoryTextView.setText(R.string.T5_End);
+                    updateAtEnd();
+                    mIndex = 5;
+                    break;
         }
     }
 }
