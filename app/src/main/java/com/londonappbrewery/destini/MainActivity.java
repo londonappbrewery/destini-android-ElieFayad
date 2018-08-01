7b81411a07e653f8e8e80d5b04b64eac1a046ae6
ElieFayad <elie.fayad98@gmail.com>
2018-08-01 12:55:50 +0300

    Section 9 challenge: completed

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,20 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    int mIndex;
+    private TextView mStoryTextView;
+    private TextView mUserPrompt;
+    private Button mTopButton;
+    private Button mBottomButton;
 
 
     @Override
@@ -15,16 +25,69 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mUserPrompt = (TextView) findViewById(R.id.userPrompt);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
+        mIndex = 1;
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateTop();
+            }
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateBottom();
+            }
+        });
 
+    }
 
+    private void updateAtEnd(){
+        mTopButton.setVisibility(View.GONE);
+        mBottomButton.setVisibility(View.GONE);
+        mUserPrompt.setText(R.string.end);
+    }
+
+    private void updateTop(){
+        switch(mIndex){
+            case 1: case 2:mStoryTextView.setText(R.string.T3_Story);
+                           mTopButton.setText(R.string.T3_Ans1);
+                           mBottomButton.setText(R.string.T3_Ans2);
+                           mIndex=3;
+                           break;
+            case 3:mStoryTextView.setText(R.string.T6_End);
+                updateAtEnd();
+                mIndex=6;
+                break;
+        }
+
+    }
+
+    private void updateBottom(){
+        switch (mIndex) {
+            case 1:
+                mStoryTextView.setText(R.string.T2_Story);
+                mTopButton.setText(R.string.T2_Ans1);
+                mBottomButton.setText(R.string.T2_Ans2);
+                mIndex = 2;
+                break;
+            case 2:
+                mStoryTextView.setText(R.string.T4_End);
+                updateAtEnd();
+                mIndex = 4;
+                break;
+            case 3:
+                mStoryTextView.setText(R.string.T5_End);
+                updateAtEnd();
+                mIndex = 5;
+                break;
+        }
     }
 }
